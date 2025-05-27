4dbb47931204307ee045dc4377e232c484afb023
Denis Zhdanov <dinis.04@yandex.ru>
2025-05-27 19:00:03 +0700

    da v pizdy

--- backend/geo-api/build.gradle.kts
+++ backend/geo-api/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
