9f4d8badf54703f25f993bf5f5baff310507ab17
Timofey Gavrikov <gavrikovtimofeysteam@gmail.com>
2025-11-10 21:56:43 +0700

    fix dock

--- frontend/mobile/lib/main.dart
+++ frontend/mobile/lib/main.dart
@@ -110,11 +110,7 @@ class _MainScreenState extends State<MainScreen> {
         setState(() {
           _currentIndex = index;
         });
-        _pageController.animateToPage(
-          index,
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeInOut,
-        );
+        _pageController.jumpToPage(index);
       },
       child: Container(
         padding: const EdgeInsets.all(12),
