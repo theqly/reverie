53f65ce823d90155f34400dd1c9790059aa80bf8
katerina <katerina@LaptopK.>
2025-11-30 00:33:14 +0700

    kafka producer

---
+++ backend/kafka-module/go.mod
@@ -0,0 +1,11 @@
+module github.com/theqly/reverie/backend/kafka-module
+
+go 1.24.0
+
+toolchain go1.24.1
+
+require (
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.49 // indirect
+)
---
+++ backend/kafka-module/producer.go
@@ -0,0 +1,31 @@
+package producer
+
+import (
+	"context"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Producer struct {
+	writer *kafka.Writer
+}
+
+func NewProducer(brokers []string) *Producer {
+	return &Producer{
+		writer: kafka.NewWriter(kafka.WriterConfig{
+			Brokers: brokers,
+		}),
+	}
+}
+
+func (p *Producer) Publish(ctx context.Context, topic string, key string, value []byte) error {
+	return p.writer.WriteMessages(ctx, kafka.Message{
+		Topic: topic,
+		Key:   []byte(key),
+		Value: value,
+	})
+}
+
+func (p *Producer) Close() error {
+	return p.writer.Close()
+}
